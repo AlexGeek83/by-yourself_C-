f66bb3322330550e2c93001f9b3a411a00b3408d
Alex <lrl.msk@yandex.ru>
2022-05-10 19:32:31 +0300

    add

---
+++ Lesson_3_Array/Program.cs
@@ -0,0 +1,21 @@
+﻿// ======= Массивы ========== //
+
+//  задать массив можно разными способами
+// ===== способ 1 ======
+int[] free = new int[10];
+// ===== способ 2 ======
+int[] free1 = {1, 5, 8, 58, 78};
+
+string stringMassiv = "Привет программист, как ты?";
+//  System.Console.WriteLine(stringMassiv.Length);  // узнали длину массива/ размер
+char firstLetter = stringMassiv[0];
+// снова узнали длину массива ниже
+int massivSize = stringMassiv.Length;
+System.Console.WriteLine(stringMassiv.Length);
+massivSize = free1.Length;
+System.Console.WriteLine(free1.Length);
+
+
+
+
+
